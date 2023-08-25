66d4c38b5b2b4cda797fe3c7b684bfff79fd1bda
xabicoloma <xabi165@gmail.com>
2023-08-24 22:28:47 -0400

    Entrega

--- taskflow/tareas/admin.py
+++ taskflow/tareas/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Etiqueta, Tarea
 
 # Register your models here.
+admin.site.register(Etiqueta)
+admin.site.register(Tarea)
--- taskflow/tareas/models.py
+++ taskflow/tareas/models.py
@@ -1,3 +1,27 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Etiqueta(models.Model):
+    tipo = models.CharField(max_length=10)
+
+    def __str__(self) -> str:
+        return self.type
+
+class Tarea(models.Model):
+    STATUS_CHOICES = [
+    ('Pendiente', 'Pendiente'),
+    ('En progreso', 'En progreso'),
+    ('Completada', 'Completada'),
+    ]
+        
+    titulo = models.CharField(max_length=50, blank=False, null=False)
+    fecha = models.DateField()
+    descripcion = models.CharField(blank=True, null=True)
+    estado = models.CharField(max_length=15, choices=STATUS_CHOICES, default='Pendiente')
+    tareas_owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    tareas_etiqueta = models.ForeignKey(Etiqueta, default=1, on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return f'{self.titulo} > {self.tareas_owner}'
+
